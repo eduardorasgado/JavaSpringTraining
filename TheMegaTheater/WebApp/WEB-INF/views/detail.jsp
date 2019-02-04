912d6e13e304142ef5a9f0d905018e5a51869837
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-02-03 23:27:24 -0600

    detail view and method in home controller to render dynamic data

---
+++ TheMegaTheater/WebApp/WEB-INF/views/detail.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Detalles: "${ titulo }"</title>
+</head>
+<body>
+	<div>
+		<h1>Pelicula: ${ titulo }</h1>
+		<h2>Duración: ${ duracion }</h2>
+		<h3>Precio: ${ precio }</h3>
+		<p>Sintesis: ${ descripcion }</p>
+	</div>
+</body>
+</html>
--- TheMegaTheater/src/main/java/com/eduardocode/app/controller/HomeController.java
+++ TheMegaTheater/src/main/java/com/eduardocode/app/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.eduardocode.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,5 +11,25 @@ public class HomeController {
 	@RequestMapping(value="/home", method=RequestMethod.GET)
 	public String index() {
 		return "home";
+	}
+	
+	
+	@RequestMapping(value="/detail", method=RequestMethod.GET)
+	public String showDetail(Model model) {
+		
+		String tituloPelicula = "El Imaginario del Dr Parnasus";
+		int duracion = 135;
+		double precioEntrada = 120;
+		String desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+				+ "Ut pellentesque libero id vehicula facilisis. Nullam blandit et "
+				+ "felis in interdum. Mauris cursus eros quis nibh faucibus condimentum. ";
+		
+		// pasando datos al frontend
+		model.addAttribute("titulo", tituloPelicula);
+		model.addAttribute("duracion", duracion);
+		model.addAttribute("precio", precioEntrada);
+		model.addAttribute("descripcion", desc);
+		
+		return "detail";
 	}
 }
