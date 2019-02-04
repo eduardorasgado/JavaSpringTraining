b1c3fc36af7d014d056011786283a4b6bbc84685
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-02-03 23:59:26 -0600

    rendering a list in home

--- TheMegaTheater/WebApp/WEB-INF/views/home.jsp
+++ TheMegaTheater/WebApp/WEB-INF/views/home.jsp
@@ -1,5 +1,10 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+
+<!-- Para usar JSTL en cada vista debemos de importar jstl en el jsp.
+ Importamos el core de JSTL-->
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,7 +17,15 @@
 		<h1>The Great Theater</h1>
 	</div>
 	<div>
-		<p>Lorem ipsum dolor sit amet, artefactatum prilisocium elit</p>
+		<h2>Cartelera</h2>
+		<p>Peliculas que tenemos en cartelera:</p>
+		
+		<ul>
+			<!--  desplegando la lista del backend haciendo uso de JSTL -->
+			<c:forEach items="${ peliculas }" var="pelicula">
+				<li>${ pelicula }</li>
+			</c:forEach>
+		</ul>
 	</div>
 </body>
 </html>
--- TheMegaTheater/src/main/java/com/eduardocode/app/controller/HomeController.java
+++ TheMegaTheater/src/main/java/com/eduardocode/app/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.eduardocode.app.controller;
 
+import java.util.LinkedList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,12 +9,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-
+	
 	@RequestMapping(value="/home", method=RequestMethod.GET)
 	public String index() {
 		return "home";
 	}
 	
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String showMain(Model model) {
+		
+		// devolviendo una lista al frontend
+		// sintaxis java 10
+		var peliculas = new LinkedList<String>();
+		
+		peliculas.add("El jardín de las palabras");
+		peliculas.add("El viaje de Chihiro");
+		peliculas.add("En este rincón del mundo");
+		peliculas.add("El quinto elemento");
+		
+		// mandando la lista al frontend
+		model.addAttribute("peliculas", peliculas);
+		
+		return "home";		
+	}
 	
 	@RequestMapping(value="/detail", method=RequestMethod.GET)
 	public String showDetail(Model model) {
@@ -23,6 +42,7 @@ public class HomeController {
 		String desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
 				+ "Ut pellentesque libero id vehicula facilisis. Nullam blandit et "
 				+ "felis in interdum. Mauris cursus eros quis nibh faucibus condimentum. ";
+		
 		
 		// pasando datos al frontend
 		model.addAttribute("titulo", tituloPelicula);
