657a2e490878cfad207b18375145a572e56f9769
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-03-08 01:04:14 -0600

    deleteAll method. Lambda function applied and sleep in thread package

--- TheMegaTheater/src/main/java/pruebasJPA/appCreate2.java
+++ TheMegaTheater/src/main/java/pruebasJPA/appCreate2.java
@@ -2,13 +2,45 @@ package pruebasJPA;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.eduardocode.app.model.Noticia;
+import com.eduardocode.app.repository.NoticiasRepository;
+
 public class appCreate2 {
 
 	public static void main(String[] args) {
-		var context = new ClassPathXmlApplicationContext("context.xml");
+		var context = new ClassPathXmlApplicationContext("root-context.xml");
 		
+		NoticiasRepository repo = context.getBean("noticiasRepository", NoticiasRepository.class);
+
 		// Contar los numeros de regustros en la tabla
+		long num = repo.count();
+		System.out.println("Existen "+ num + " registros en la tabla Noticias");
 		
+		// Encontrando todos los elementos de una tabla
+		Iterable<Noticia> noticias = repo.findAll();
+		// podemos iterar entre los objetos ahora
+		// uso de funciones lambda en java 11
+		noticias.forEach((noticia) -> 
+			{ System.out.println(noticia.getTitulo()); 
+		});
+		
+		System.out.println("Borrando todos los elementos de la tabla noticia");
+		// Metodo DeleteAll -> Eliminar todos los registros de una tabla
+		repo.deleteAll();
+		
+		long start = System.currentTimeMillis();
+		// esperamos 6 segundos antes de volver a recuperar todos los elementos de nuevo
+		try {
+			Thread.sleep(6000);
+		} catch(InterruptedException e) {
+			System.out.println("La operacion ha sido interrumpida");
+		}
+		// imprimimos en pantalla el total de tiempo real que se tomo el sleep
+		System.out.println("Sleep time in ms is:" + (System.currentTimeMillis() - start));
+		
+		// volvemos a incluir todos los elementos de nuevo en la tabla
+		noticias.forEach((noticia) ->
+					{ repo.save(noticia); } );
 		
 		context.close();
 	}
