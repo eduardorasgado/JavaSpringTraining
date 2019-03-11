2bee88d98121ac9b01fd724b0368c2e57461898b
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-03-10 23:27:59 -0600

    Sorting a pagination

---
+++ TheMegaTheater/src/main/java/pruebasJPARepository/AppPaginationSorted.java
@@ -0,0 +1,46 @@
+package pruebasJPARepository;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import com.eduardocode.app.model.Noticia;
+import com.eduardocode.app.repository.NoticiasRepository;
+
+public class AppPaginationSorted {
+
+	public static void main(String[] args) {
+		var context = new ClassPathXmlApplicationContext("root-context.xml");
+		var repoNoticias = context.getBean("noticiasRepository", NoticiasRepository.class);
+		
+		// Obtener todas las entidades por paginacion
+		// recuperando las paginas de la 1 a la 7
+		showPage(repoNoticias, 0, 5);
+		showPage(repoNoticias, 1, 5);
+		showPage(repoNoticias, 2, 5);
+		showPage(repoNoticias, 3, 5);
+		showPage(repoNoticias, 4, 5);
+		showPage(repoNoticias, 5, 5);
+		showPage(repoNoticias, 6, 5);
+		
+		context.close();
+	}
+	
+	public static void showPage(NoticiasRepository repoNoticias, int page, int items) {
+		
+		// en el pageable tiene que ir el tipo de ordenamiento
+		var pageable = PageRequest.of(page, items, 
+				Sort.by("titulo").descending());  // num de pagina, num de registros, 
+																		// tipo de ordenamiento 
+		Page<Noticia> pagina = repoNoticias.findAll(pageable);
+		
+		// imprimiendo los elementos contenidos en la primera pagina
+		System.out.println("[PAGINA "+ (page+1)+ "]");
+		
+		pagina.forEach( (noticia) -> {
+			System.out.println(noticia);
+		});
+	}
+
+}
