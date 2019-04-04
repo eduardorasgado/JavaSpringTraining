0c9df50e23bbc6dc8731faa995da75f06e25b9a7
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-04-04 12:03:10 -0600

    Service tags to perfiles and usuarios services. save method completed at usuario controller.

--- TheMegaTheater/src/main/java/com/eduardocode/app/controller/UsuariosController.java
+++ TheMegaTheater/src/main/java/com/eduardocode/app/controller/UsuariosController.java
@@ -11,9 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.eduardocode.app.model.Perfil;
 import com.eduardocode.app.model.Usuario;
+import com.eduardocode.app.service.IPerfilesService;
+import com.eduardocode.app.service.IUsuariosService;
 
 @Controller
 @RequestMapping("/usuarios")
@@ -23,6 +27,12 @@ public class UsuariosController {
 	// con spring security
 	@Autowired
 	private BCryptPasswordEncoder encoder;
+	
+	@Autowired
+	private IUsuariosService usuariosService;
+	
+	@Autowired
+	private IPerfilesService perfilesService;
 	
 	@GetMapping("/index")
 	public String index(Model model) {
@@ -44,10 +54,28 @@ public class UsuariosController {
 	}
 	
 	@PostMapping("/save")
-	public String save(@ModelAttribute Usuario usuario, Model model,
+	public String save(@ModelAttribute Usuario usuario, 
+			@RequestParam("perfil") String perfil,
 			RedirectAttributes attributes) {
-		//
+		
 		System.out.println(usuario);
+		System.out.println(perfil);
+		
+		// guardar los objetos en la base de datos
+		String tempPass = usuario.getPassword(); // pass en texto plano
+		String cryptPass = encoder.encode(tempPass); // encriptando la pass
+		
+		usuario.setPassword(cryptPass);
+		
+		usuariosService.insert(usuario);
+		
+		// definimos el rol
+		Perfil tempPerfil = new Perfil();
+		tempPerfil.setUsername(usuario.getUsername());
+		tempPerfil.setPerfil(perfil);
+		
+		perfilesService.insert(tempPerfil);
+		
 		attributes.addFlashAttribute("message", "Se ha guardado el usuario exitosamente");
 		return "redirect:/usuarios/index";
 	}
